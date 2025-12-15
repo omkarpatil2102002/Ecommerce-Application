d4fa396139e84af70732a6dfbef6f5525c836d71
Omkar Patil <128567958+omkarpatil2102002@users.noreply.github.com>
2025-12-15 23:12:54 +0530

    Add files via upload

---
+++ cart-page.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { cart, priceSummary } from '../data-type';
+import { Product } from '../services/product';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cart-page',
+  imports: [],
+  templateUrl: './cart-page.html',
+  styleUrl: './cart-page.css'
+})
+export class CartPage {
+  cartData: cart[] | undefined;
+  priceSummary: priceSummary = {
+    price: 0,
+    discount: 0,
+    tax: 0,
+    delivery: 0,
+    total: 0
+  }
+  constructor(private product: Product, private router: Router) { }
+  
+  ngOnInit(): void {
+   this.loadDetails()
+
+  }
+
+  removeToCart(cartId:number|undefined){
+    cartId && this.cartData && this.product.removeToCart(cartId)
+    .subscribe((result)=>{
+      this.loadDetails();
+    })
+  }
+
+  loadDetails(){
+    this.product.currentCart().subscribe((result) => {
+      this.cartData = result;
+      console.warn(this.cartData);
+      let price = 0;
+      result.forEach((item) => {
+        if (item.quantity) {
+          price = price + (+item.price * +item.quantity)
+        }
+      })
+      this.priceSummary.price = price;
+      this.priceSummary.discount = price / 10;
+      this.priceSummary.tax = price / 10;
+      this.priceSummary.delivery = 100;
+      this.priceSummary.total = price + (price / 10) + 100 - (price / 10);
+
+    if(!this.cartData.length){
+      this.router.navigate(['/'])
+    }
+
+    })
+  }
+
+
+
+
+  checkout() {
+    this.router.navigate(['/checkout'])
+  }
+
+
+}
